1c2cd66504f640cd1cc66cee8875ed25da38e24b
Sen <nevyn202@outlook.com>
2023-03-12 22:30:16 -0500

    Better Start Scripts

--- start.bat
+++ start.bat
@@ -1,4 +1,6 @@
 @echo off
 
+git pull
+
 start cmd /k "cd frontend && npm i && npm run dev"
-start cmd /k "cd backend && python server.py"
+start cmd /k "cd backend && pip install -r requirements.txt && python server.py"
