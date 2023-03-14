48b70d6159c2312d9387539c27da6dee1cced057
peeepy <111094949+peeepy@users.noreply.github.com>
2023-03-14 06:03:49 +0000

    fix stuff

--- frontend/src/assets/components/menucomponents/Modal.jsx
+++ frontend/src/assets/components/menucomponents/Modal.jsx
@@ -3,17 +3,26 @@ import "./modalStyles.css";
 
 function Modal({ children, isOpen, handleClose }) {
   useEffect(() => {
+    
     const closeOnEscapeKey = e => e.key === "Escape" ? handleClose() : null;
+    const closeOnOutsideClick = e => {
+      if (modalRef.current && !modalRef.current.contains(e.target)) {
+        handleClose();
+      }
+    };
     document.body.addEventListener("keydown", closeOnEscapeKey);
+    window.addEventListener("mousedown", closeOnOutsideClick);
+  
     return () => {
       document.body.removeEventListener("keydown", closeOnEscapeKey);
+      window.removeEventListener("mousedown", closeOnOutsideClick);
     };
-  }, [handleClose]);
+  }, []);  
 
   if (!isOpen) return null;
 
   return (
-    <div className="modal-overlay">
+    <div className="modal">
       <button onClick={handleClose} className="close-btn">
         Close
       </button>
